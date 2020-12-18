cc24a16e7465adbb1183d8ccc6d03986b6d98fbb
techtorial <info@techtorialacademy.com>
2020-12-18 11:07:00 -0600

    JENKINS: add junit

--- src/test/java/api/stepdefs/APITestStepDefs.java
+++ src/test/java/api/stepdefs/APITestStepDefs.java
@@ -8,7 +8,7 @@ import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.testng.Assert;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Map;
